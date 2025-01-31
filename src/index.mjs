d9b1f5e3fd1269b380b25a084a6c112a9ee24740
Tyler Liu <tyler4long@gmail.com>
2025-01-31 15:27:32 -0800

    Add test3

---
+++ src/index.mjs
@@ -0,0 +1,23 @@
+import RingCentral from "@rc-ex/core";
+import WSExt from "@rc-ex/ws";
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+  clientId: process.env.RINGCENTRAL_CLIENT_ID,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN,
+  });
+  const ws = new WSExt({
+    restOverWebSocket: true,
+    debugMode: true,
+  });
+  await rc.installExtension(ws);
+  const r = await rc.restapi().account().extension().get();
+  console.log(JSON.stringify(r, null, 2));
+  await rc.revoke();
+};
+main();
