529dcd61c2e816e97d749a64f197fae79e6361d5
Tyler Liu <tyler4long@gmail.com>
2025-02-03 17:03:54 -0800

    Try latest version

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,7 @@
 import RingCentral from "@rc-ex/core";
 import WSExt from "@rc-ex/ws";
 
-const rc = new RingCentral({
+const rc = new RingCentral.default({
   server: process.env.RINGCENTRAL_SERVER_URL,
   clientId: process.env.RINGCENTRAL_CLIENT_ID,
   clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET,
@@ -11,7 +11,7 @@ const main = async () => {
   await rc.authorize({
     jwt: process.env.RINGCENTRAL_JWT_TOKEN,
   });
-  const ws = new WSExt({
+  const ws = new WSExt.default({
     restOverWebSocket: true,
     debugMode: true,
   });
